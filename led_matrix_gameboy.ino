4ba2066dd986cbf16c2fc7531bf95d2b59fdf95d
Sebastian <uhl.sebastian@gmail.com>
2020-02-28 15:44:09 +0100

    clean basic I/O demo

--- led_matrix_gameboy.ino
+++ led_matrix_gameboy.ino
@@ -4,6 +4,8 @@
 #include <WS2812.h>
 
 #define LEDCount 64
+
+//button mapping
 #define ledDataPin 7
 #define a_but 3
 #define b_but 4
@@ -14,13 +16,11 @@
 
 WS2812 LED(LEDCount);
 cRGB value;
-
 int h = 0;   //stores 0 to 614
-byte steps = 55; //number of hues we skip in a 360 range per update
 
-byte sat = 255;//100;
-byte val = 10;
-
+byte steps = 5; //number of hues we skip in a 360 range per update
+byte sat = 255;
+byte val = 20; //brightness
 long sleep = 50; //delays between update
 
 void setup() {
@@ -41,30 +41,25 @@ void setup() {
 
 void loop() {
   uint64_t time_now = millis();
-  int static s = 0;
-  s++;
-  int static state;
-  if(s == 64)
-    state+=steps;
-  state %= 360;
-  s %= 64;
 
   for(int i = 0; i < LEDCount; i++)
   {
-    value.SetHSV(state, sat, 0);
+    h += steps;
+    h %= 360;
+    value.SetHSV(h, sat, 0);
 
     if(i/8 == a_but-3 && digitalRead(a_but) == LOW)
-      value.SetHSV(state, sat, val);
+      value.SetHSV(h, sat, val);
     else if(i/8 == b_but-3 && digitalRead(b_but) == LOW)
-      value.SetHSV(state, sat, val);
+      value.SetHSV(h, sat, val);
     else if(i/8 == up_but-3 && digitalRead(up_but) == LOW)
-      value.SetHSV(state, sat, val);
+      value.SetHSV(h, sat, val);
     else if(i/8 == down_but-3 && digitalRead(down_but) == LOW)
-      value.SetHSV(state, sat, val);
+      value.SetHSV(h, sat, val);
     else if(i/8 == left_but-3 && digitalRead(left_but) == LOW)
-      value.SetHSV(state, sat, val);
+      value.SetHSV(h, sat, val);
     else if(i/8 == right_but-3 && digitalRead(right_but) == LOW)
-      value.SetHSV(state, sat, val);
+      value.SetHSV(h, sat, val);
 
     LED.set_crgb_at(i, value);
 
@@ -73,11 +68,4 @@ void loop() {
   LED.sync();
   while(millis() < time_now + sleep)
     ;
-}
-
-unsigned int rng(){
-  uint64_t static seed = 1000;
-  seed = ((seed * 344534537681)+4534535809)%875644564849;
-  //seed = ((seed * 23)+7)%1012;
-  return (unsigned int) seed % 360;
 }
