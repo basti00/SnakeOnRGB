c5878d20d219ccf550d4b0559570c2d82e06f89d
Sebastian <uhl.sebastian@gmail.com>
2020-02-29 16:51:14 +0100

    snake: drawing board and basic movement

--- led_matrix_gameboy.ino
+++ led_matrix_gameboy.ino
@@ -3,9 +3,22 @@
 
 #include <WS2812.h>
 
-#define LEDCount 64
+#define RB_HUE_STEPS 10
+#define GLOBAL_SAT 255
+#define GLOBAL_BRGHT 50
+#define frame_tick 20 //delays between frameupdates
+#define game_tick 400 //delays between game updates
 
-//button mapping
+/*
+ * board definitions
+ */
+#define LEDHeight 8
+#define LEDWidth 8
+#define LEDCount (LEDHeight * LEDWidth)
+
+/*
+ * button mapping
+ */
 #define ledDataPin 7
 #define a_but 3
 #define b_but 4
@@ -14,14 +27,38 @@
 #define right_but 8
 #define left_but 9
 
-WS2812 LED(LEDCount);
-cRGB value;
-int h = 0;   //stores 0 to 614
+/*
+ * typedefs
+ */
+typedef enum{
+  A = 0,
+  B,
+  UP,
+  DOWN,
+  RIGHT,
+  LEFT
+} key;
 
-byte steps = 5; //number of hues we skip in a 360 range per update
-byte sat = 255;
-byte val = 20; //brightness
-long sleep = 50; //delays between update
+typedef enum{
+  X_ = 0,
+  Y_
+} cord;
+
+typedef enum{
+  BLANK = 0,
+  RB,
+  RED,
+  GREEN,
+  BLUE
+} color;
+
+/*
+ * globals
+ */
+WS2812 LED(LEDCount);
+cRGB pixel;
+cRGB blank;
+uint8_t board[LEDHeight][LEDWidth];
 
 void setup() {
   pinMode(a_but, INPUT_PULLUP);
@@ -30,42 +67,198 @@ void setup() {
   pinMode(down_but, INPUT_PULLUP);
   pinMode(left_but, INPUT_PULLUP);
   pinMode(right_but, INPUT_PULLUP);
-
   LED.setOutput(ledDataPin);
+  Serial.begin(115200);
+
+  blank.r=0;
+  blank.g=0;
+  blank.b=0;
+
+
+  int heartcolor = RB;
+  board[3][1] = heartcolor;
+  board[4][1] = heartcolor;
+  board[2][2] = heartcolor;
+  board[5][2] = heartcolor;
+  board[1][3] = heartcolor;
+  board[6][3] = heartcolor;
+  board[0][4] = heartcolor;
+  board[7][4] = heartcolor;
+  board[0][5] = heartcolor;
+  board[7][5] = heartcolor;
+  board[0][6] = heartcolor;
+  board[7][6] = heartcolor;
+  board[3][6] = heartcolor;
+  board[4][6] = heartcolor;
+  board[1][7] = heartcolor;
+  board[2][7] = heartcolor;
+  board[5][7] = heartcolor;
+  board[6][7] = heartcolor;
+  while(millis()<3000){
+    renderBoard();
+    delay(frame_tick);
+  }
+  memset(board, 0, sizeof(board));
+}
+
+void renderBoard(){
+  int x=0, y=0;
+  int static hue = 0;   //stores 0 to 614
+  hue += RB_HUE_STEPS;
+  hue %= 360;
+
   for(int i = 0; i < LEDCount; i++)
   {
-    value.SetHSV(h, sat, 0);
-    LED.set_crgb_at(i, value);
+    if(board[x][LEDWidth-1-y] == RB){
+      pixel.SetHSV((hue+i*360/9)%360, GLOBAL_SAT, GLOBAL_BRGHT);
+      LED.set_crgb_at(i, pixel);
+    }
+    else if(board[x][LEDWidth-1-y] == RED){
+      pixel.SetHSV(0, GLOBAL_SAT, GLOBAL_BRGHT);
+      LED.set_crgb_at(i, pixel);
+    }
+    else if(board[x][LEDWidth-1-y] == GREEN){
+      pixel.SetHSV(120, GLOBAL_SAT, GLOBAL_BRGHT);
+      LED.set_crgb_at(i, pixel);
+    }
+    else if(board[x][LEDWidth-1-y] == BLUE){
+      pixel.SetHSV(240, GLOBAL_SAT, GLOBAL_BRGHT);
+      LED.set_crgb_at(i, pixel);
+    }
+    else
+    {
+      LED.set_crgb_at(i, blank);
+    }
+    x++;
+    if(x==LEDHeight){
+      x=0;
+      y++;
+    }
+  }
+  LED.sync();
+}
+
+uint8_t getInput(){
+  uint8_t inp = 0;
+  inp |= !digitalRead(a_but)<<A;
+  inp |= !digitalRead(b_but)<<B;
+  inp |= !digitalRead(up_but)<<UP;
+  inp |= !digitalRead(down_but)<<DOWN;
+  inp |= !digitalRead(right_but)<<RIGHT;
+  inp |= !digitalRead(left_but)<<LEFT;
+  return inp;
+}
+
+void bin_print(uint8_t b){
+  Serial.print("0b");
+  for(int i=0; i<8; i++){
+    if( b & (1<<(7-i)) )
+      Serial.print("1");
+    else
+      Serial.print("0");
   }
 }
 
-void loop() {
-  uint64_t time_now = millis();
+bool inline input(uint8_t in, uint8_t button){
+  return (in & (1 << button));
+}
 
-  for(int i = 0; i < LEDCount; i++)
-  {
-    h += steps;
-    h %= 360;
-    value.SetHSV(h, sat, 0);
+void snake(uint8_t in){
+  int static vel[2] = {0,1};
+  int static pos[2] = {2,2};
+  int new_pos[2];
 
-    if(i/8 == a_but-3 && digitalRead(a_but) == LOW)
-      value.SetHSV(h, sat, val);
-    else if(i/8 == b_but-3 && digitalRead(b_but) == LOW)
-      value.SetHSV(h, sat, val);
-    else if(i/8 == up_but-3 && digitalRead(up_but) == LOW)
-      value.SetHSV(h, sat, val);
-    else if(i/8 == down_but-3 && digitalRead(down_but) == LOW)
-      value.SetHSV(h, sat, val);
-    else if(i/8 == left_but-3 && digitalRead(left_but) == LOW)
-      value.SetHSV(h, sat, val);
-    else if(i/8 == right_but-3 && digitalRead(right_but) == LOW)
-      value.SetHSV(h, sat, val);
+  //memset(board, 0, sizeof(board));
 
-    LED.set_crgb_at(i, value);
-
+  /*
+   * inputs
+   */
+  if(input(in,UP)){
+    vel[Y_] = 1;
+    vel[X_] = 0;
+  }
+  if(input(in,DOWN)){
+    vel[Y_] = -1;
+    vel[X_] = 0;
+  }
+  if(input(in,RIGHT)){
+    vel[Y_] = 0;
+    vel[X_] = 1;
+  }
+  if(input(in,LEFT)){
+    vel[Y_] = 0;
+    vel[X_] = -1;
   }
 
-  LED.sync();
-  while(millis() < time_now + sleep)
+  /*
+   * translation
+   */
+  new_pos[X_] = pos[X_]+vel[X_];
+  new_pos[Y_] = pos[Y_]+vel[Y_];
+
+
+  /*
+   * bounding box
+   */
+  if(new_pos[X_] < 0){
+    new_pos[X_] = 0;
+  }
+  if(new_pos[Y_] < 0){
+    new_pos[Y_] = 0;
+  }
+  if(new_pos[X_] >= LEDWidth){
+    new_pos[X_] = pos[X_];
+  }
+  if(new_pos[Y_] >= LEDHeight){
+    new_pos[Y_] = pos[Y_];
+  }
+
+  pos[X_] = new_pos[X_];
+  pos[Y_] = new_pos[Y_];
+  board[new_pos[0]][new_pos[1]] = GREEN;
+}
+
+bool task(int period){
+  uint64_t static time_start = 0;
+  if(millis() >= time_start + period){
+    time_start = millis();
+    return true;
+  }
+  return false;
+}
+
+void loop() {
+  uint64_t time_start = millis();
+  uint8_t static in;
+  renderBoard();
+  in |= getInput();
+
+  if (task(game_tick)){
+    bin_print(in);
+    Serial.println();
+    snake(in);
+    in = 0;
+  }
+
+  /*
+   * timing
+   */
+  uint64_t time_end = millis();
+  if((time_end-time_start)>frame_tick*7/10)
+  {
+    Serial.print(" flow: ");
+    Serial.print((int)((time_end-time_start)*100)/frame_tick);
+    Serial.println("%");
+  }
+  if((time_end-time_start)>frame_tick)
+  {
+    Serial.println(" REALTIME VIOLATION");
+    pixel.SetHSV(0, 255, GLOBAL_BRGHT); //red
+    for(int i=0; i<64; i++){
+      LED.set_crgb_at(i, pixel);
+    }
+    LED.sync();
+  }
+  while(millis() < time_start + frame_tick)
     ;
 }
